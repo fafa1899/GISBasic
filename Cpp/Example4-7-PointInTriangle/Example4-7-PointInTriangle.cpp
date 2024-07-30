0c9209dba27a857833649c386f53914ba36a1a89
fafa1899 <holybell44@126.com>
2024-07-30 20:19:59 +0800

    [A]增加新的案例4.4~4.8
    Signed-off-by: fafa1899 <holybell44@126.com>

---
+++ Cpp/Example4-7-PointInTriangle/Example4-7-PointInTriangle.cpp
@@ -0,0 +1,49 @@
+﻿// Example7PointInTriangle.cpp : 此文件包含 "main"
+// 函数。程序执行将在此处开始并结束。
+//
+
+#include <Eigen/Eigen>
+#include <iostream>
+
+using namespace Eigen;
+using namespace std;
+
+using Triangle = Vector3d[3];
+
+// 判断点P是否在空间三角形内
+bool PointInTriangle3D(Triangle triangle, Vector3d& P) {
+  auto v0p = P - triangle[0];
+  auto v0v1 = triangle[1] - triangle[0];
+  auto v0v2 = triangle[2] - triangle[0];
+
+  double D = v0v1.x() * v0v2.y() - v0v1.y() * v0v2.x();
+  if (D == 0.0) {
+    return false;
+  }
+
+  double D1 = v0p.x() * v0v2.y() - v0p.y() * v0v2.x();
+  double D2 = v0v1.x() * v0p.y() - v0v1.y() * v0p.x();
+
+  double u = D1 / D;
+  double v = D2 / D;
+
+  //如果在三维空间判断，还需要判断第三个向量是否成立
+  // double eps = v0v1.z() * u + v0v2.z() * v - P.z();
+
+  if (u >= 0 && v >= 0 && u + v <= 1) {
+    return true;
+  }
+
+  return false;
+}
+
+int main() {
+  Triangle triangle = {Vector3d(-20, -25.6, 0), Vector3d(34, -24, 0),
+                       Vector3d(30, 27, 0)};
+
+  Vector3d A(1.2, 3.4, 0);
+  cout << "点A在三角形内：" << PointInTriangle3D(triangle, A) << endl;
+
+  Vector3d B(14.4, 8.14, 0);
+  cout << "点B在三角形内：" << PointInTriangle3D(triangle, B) << endl;
+}
---
+++ Cpp/Example4-8-PointInPolygon/Example4-8-PointInPolygon.cpp
@@ -0,0 +1,131 @@
+﻿// Example8PointInPolygon.cpp : 此文件包含 "main"
+// 函数。程序执行将在此处开始并结束。
+//
+
+#include <Eigen/Eigen>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+#define EPSILON 0.000001
+
+using namespace Eigen;
+using namespace std;
+
+using Polygon = vector<Vector2d>;
+
+const double epsilon = 0.000000001;
+
+//判断点在线段上
+bool PointInLine(const Vector2d& point, const Vector2d& startPoint,
+                 const Vector2d& endPoint) {
+  Vector3d P1P2;
+  P1P2 << endPoint - startPoint, 0;
+  Vector3d P1P;
+  P1P << point - startPoint, 0;
+
+  if (fabs((P1P2.cross(P1P)).norm()) > epsilon) {
+    return false;
+  }
+
+  double dotProduct = P1P2.dot(P1P);
+  if (dotProduct > 0 && dotProduct < P1P2.squaredNorm()) {
+    return true;
+  }
+
+  return false;
+}
+
+//两条线段相交
+bool LineIntersection(const Vector2d& startPoint1, const Vector2d& endPoint1,
+                      const Vector2d& startPoint2, const Vector2d& endPoint2) {
+  Vector2d direction1 = endPoint1 - startPoint1;
+  Vector2d direction2 = endPoint2 - startPoint2;
+
+  double D = -direction1.x() * direction2.y() + direction1.y() * direction2.x();
+  if (fabs(D) < epsilon) {
+    return false;
+  }
+
+  Vector2d O12 = startPoint2 - startPoint1;
+  double D1 = -O12.x() * direction2.y() + O12.y() * direction2.x();
+  double D2 = direction1.x() * O12.y() - direction1.y() * O12.x();
+
+  double t1 = D1 / D;
+  if (t1 < 0 || t1 > 1) {
+    return false;
+  }
+
+  double t2 = D2 / D;
+  if (t2 < 0 || t2 > 1) {
+    return false;
+  }
+
+  return true;
+}
+
+//判断点在多边形内
+bool Point_In_Polygon_2D(const Vector2d& point, const Polygon& polygon) {
+  bool isInside = false;
+  int count = 0;
+
+  double minX = DBL_MAX;
+  for (int i = 0; i < polygon.size(); i++) {
+    minX = std::min(minX, polygon[i].x());
+  }
+
+  const Vector2d& testRayLineStart = point;
+  Vector2d testRayLineEnd(minX - 10,
+                          point.y());  //取最小的X值还小的值作为射线的终点
+
+  //遍历每一条边
+  for (int i = 0; i < polygon.size() - 1; i++) {
+    if (PointInLine(point, polygon[i], polygon[i + 1])) {
+      return true;
+    }
+
+    if (PointInLine(polygon[i], testRayLineStart, testRayLineEnd)) {
+      if (polygon[i].y() > polygon[i + 1].y())  //只保证上端点+1
+      {
+        count++;
+      }
+    } else if (PointInLine(polygon[i + 1], testRayLineStart, testRayLineEnd)) {
+      if (polygon[i + 1].y() > polygon[i].y())  //只保证上端点+1
+      {
+        count++;
+      }
+    } else if (LineIntersection(polygon[i], polygon[i + 1], testRayLineStart,
+                                testRayLineEnd)) {
+      count++;
+    }
+  }
+
+  if (count % 2 == 1) {
+    isInside = true;
+  }
+
+  return isInside;
+}
+
+int main() {
+  //定义一个多边形（六边形）
+  Polygon polygon;
+  polygon.emplace_back(268.28, 784.75);
+  polygon.emplace_back(153.98, 600.60);
+  polygon.emplace_back(274.63, 336.02);
+  polygon.emplace_back(623.88, 401.64);
+  polygon.emplace_back(676.80, 634.47);
+  polygon.emplace_back(530.75, 822.85);
+  polygon.emplace_back(268.28, 784.75);  //将起始点放入尾部，方便遍历每一条边
+
+  Vector2d a(407.98, 579.43);
+  cout << "点（407.98, 579.43）是否在多边形内："
+       << Point_In_Polygon_2D(a, polygon) << endl;
+
+  Vector2d b(678.92, 482.07);
+  cout << "点（678.92, 482.07）是否在多边形内："
+       << Point_In_Polygon_2D(b, polygon) << endl;
+
+  return 0;
+}
