658e6f851da0f67a10024674b0b555b0a50bf252
fafa1899 <holybell44@126.com>
2024-08-12 10:36:52 +0800

    [D]删除旧代码

--- Code/JavaScriptWork/server.js
+++
@@ -1,8 +0,0 @@
-'use strict'//使用严格的javascript语法
-
-const express = require('express')
-const app = express()
-app.use(express.static('./'))  // 注意相对路径不能写错了
-app.listen(8000,()=>{
-    console.log('Server is running at http://127.0.0.1:8000')
-})
