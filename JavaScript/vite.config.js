9210b57cf2feb054f12c13cc036d31b824bcb8d5
fafa1899 <holybell44@126.com>
2024-08-13 10:29:32 +0800

    [A]案例8.1

--- JavaScript/vite.config.js
+++ JavaScript/vite.config.js
@@ -3,7 +3,13 @@ import cesium from 'vite-plugin-cesium';
 
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/geoserver': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
   },
   plugins: [cesium()]
 });
